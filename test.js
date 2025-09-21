320991343b41837463747c85d8a27ce3be02c751
root <root@DESKTOP-5ASUN5C.localdomain>
2025-09-21 23:11:39 +0800

    基础日志工具完成

---
+++ test.js
@@ -0,0 +1,8 @@
+import TzLogger from "./index.js";
+
+const logger = new TzLogger();
+
+logger.errorLogger("这是错误的日志示例！");
+logger.successLogger("这是成功的日志示例！");
+logger.infoLogger("这是提示信息的日志示例！");
+logger.warnLogger("这是警告的日志示例！");
