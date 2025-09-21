0eff6594c6837b169bd3184f15a23df342258833
root <root@DESKTOP-5ASUN5C.localdomain>
2025-09-22 00:25:07 +0800

    remove error

--- src/use.js
+++
@@ -1,9 +0,0 @@
-
-import TzLogger from 'tz-logger';
-
-const logger = new TzLogger();
-
-logger.errorLogger("这是错误的日志示例！");
-logger.successLogger("这是成功的日志示例！");
-logger.infoLogger("这是提示信息的日志示例！");
-logger.warnLogger("这是警告的日志示例！");
