f6efb7608b3d89cb2b1b9593d71d12936de30239
tz <tz18609292528@163.com>
2025-09-23 22:57:03 +0800

    delete code

--- src/index.js
+++ src/index.js
@@ -1,46 +1,47 @@
-// 生气 😤
-class TzLogger
-{
+class LoggerConfig {
     // 出错配置
-    #errorTextColor = "31";
-    #errorBgColor = "41";
-    #errorIcon = "😤";
-    #errorPreText = "执行错误日志：";
+    errorTextColor = "31";
+    errorBgColor = "41";
+    errorIcon = "😤";
+    errorPreText = "执行错误日志：";
 
     // 警告配置
-    #warnTextColor = "33";
-    #warnBgColor = "43";
-    #warnIcon = "🤔";
-    #warnPreText = "执行警告日志：";
+    warnTextColor = "33";
+    warnBgColor = "43";
+    warnIcon = "🤔";
+    warnPreText = "执行警告日志：";
 
     // 成功配置
-    #successTextColor = "32";
-    #successBgColor = "42";
-    #successIcon = "😃";
-    #successPreText = "执行成功日志：";
+    successTextColor = "32";
+    successBgColor = "42";
+    successIcon = "😃";
+    successPreText = "执行成功日志：";
 
     // 信息配置
-    #infoTextColor = "34";
-    #infoBgColor = "44";
-    #infoIcon = "🙂";
-    #infoPreText = "执行信息日志：";
+    infoTextColor = "34";
+    infoBgColor = "44";
+    infoIcon = "🙂";
+    infoPreText = "执行信息日志：";
 
-    getTimestamp()
-    {
+    // 请求配置
+    requestTextColor = "36";
+    requestBgColor = "46";
+    requestIcon = "🙂";
+    requestPreText = "发送请求日志：";
+}
+
+class TzLogger extends LoggerConfig {
+    // 获取当前时间
+    getTimestamp() {
         return new Date().toLocaleString();
     }
 
     // 柯里化定义logger
-    loggerDefine(bgColor)
-    {
-        return function loggerDefineTextColor(textColor)
-        {
-            return function loggerDefineIcon(icon)
-            {
-                return function loggerDefinePreText(preText)
-                {
-                    return function log(text)
-                    {
+    loggerDefine(bgColor) {
+        return function loggerDefineTextColor(textColor) {
+            return function loggerDefineIcon(icon) {
+                return function loggerDefinePreText(preText) {
+                    return function log(text) {
                         console.log(icon, `\x1b[${bgColor}m${preText}\x1b[0m`, `\x1b[${textColor}m${text || "未输入日志内容"}\x1b[0m`, `[${this.getTimestamp()}]`);
                     };
                 };
@@ -49,16 +50,19 @@ class TzLogger
     }
 
     // 打印错误日志
-    errorLogger = this.loggerDefine(this.#errorBgColor)(this.#errorTextColor)(this.#errorIcon)(this.#errorPreText);
+    errorLogger = this.loggerDefine(this.errorBgColor)(this.errorTextColor)(this.errorIcon)(this.errorPreText);
 
     // 打印警告日志
-    warnLogger = this.loggerDefine(this.#warnBgColor)(this.#warnTextColor)(this.#warnIcon)(this.#warnPreText);
+    warnLogger = this.loggerDefine(this.warnBgColor)(this.warnTextColor)(this.warnIcon)(this.warnPreText);
 
     // 打印成功日志
-    successLogger = this.loggerDefine(this.#successBgColor)(this.#successTextColor)(this.#successIcon)(this.#successPreText);
+    successLogger = this.loggerDefine(this.successBgColor)(this.successTextColor)(this.successIcon)(this.successPreText);
 
     // 打印信息日志
-    infoLogger = this.loggerDefine(this.#infoBgColor)(this.#infoTextColor)(this.#infoIcon)(this.#infoPreText);
+    infoLogger = this.loggerDefine(this.infoBgColor)(this.infoTextColor)(this.infoIcon)(this.infoPreText);
+
+    // 打印请求日志
+    requestLogger = this.loggerDefine(this.requestBgColor)(this.requestTextColor)(this.requestIcon)(this.requestPreText);
 }
 
 export { TzLogger };
--- test/index.test.js
+++ test/index.test.js
@@ -1,27 +1,9 @@
 import { TzLogger } from '../src/index.js';
-import { describe, it, expect } from 'vitest';
 const logger = new TzLogger();
 
-describe('logger', () =>
-{
 
-    it('错误日志打印测试', () =>
-    {
-        expect(logger.errorLogger("这是错误的日志示例！"));
-    });
-
-    it('成功日志打印测试', () =>
-    {
-        expect(logger.successLogger("这是成功的日志示例！"));
-    });
-
-    it('信息日志打印测试', () =>
-    {
-        expect(logger.infoLogger("这是提示信息的日志示例！"));
-    });
-
-    it('警告日志打印测试', () =>
-    {
-        expect(logger.warnLogger("这是警告的日志示例！"));
-    });
-});
+logger.errorLogger("这是错误的日志示例！");
+logger.successLogger("这是成功的日志示例！");
+logger.infoLogger("这是提示信息的日志示例！");
+logger.warnLogger("这是警告的日志示例！");
+logger.requestLogger("这是发送请求的日志示例！");
